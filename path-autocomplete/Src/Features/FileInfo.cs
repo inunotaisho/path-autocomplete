1fe74455d033986a0a0f5bb9c3a67ebb3e4df7d0
inunotaisho26 <ebfairweather@gmail.com>
2018-04-24 12:31:57 -0400

    putting files in proper place

--- path-autocomplete/Src/Features/FileInfo.cs
+++ path-autocomplete/Src/Features/FileInfo.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.IO;
 namespace pathautocomplete.Src.Features
 {
     public class FileInfo
     {
+        private string type;
+        private string nameExt;
+        private string nameNoExt;
+        private string itemPath;
+
         public FileInfo()
         {
+            var loc = '';
+            this.itemPath = itemPath;
+            this.type = fs.statSync(itemPath).isDirectory() ? 'dir' : 'file';
+            this.nameExt = Path.GetFileName(itemPath);
+            this.nameNoExt = Path.GetFileNameWithoutExtension(itemPath);
+        }
+
+        bool isDirectory()
+        {
+            return this.type == 'dir';
+        }
+
+        string getPath()
+        {
+            return this.itemPath;
+        }
+
+        string getName()
+        {
+            return (this.nameExt, this.nameNoExt);
         }
     }
 }
--- path-autocomplete/Src/Features/PathConfiguration.cs
+++ path-autocomplete/Src/Features/PathConfiguration.cs
@@ -4,22 +4,13 @@ namespace pathautocomplete.Src.Features
 {
     public class PathConfiguration
     {
-        private string type;
-        private string nameExt;
-        private string nameNoExt;
-        private string itemPath;
+        
 
         public PathConfiguration()
         {
-            this.itemPath = itemPath;
-            this.type = FileS ;
-            this.nameExt = Path.GetFileName(itemPath);
-            this.nameNoExt = Path.GetFileNameWithoutExtension(itemPath);
+            
         }
 
-        bool isDirectory(){
-            return this.type == 'dir';
-        }
     }
 }
 
