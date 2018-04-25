ca19d690b1c779f31b26ff8ba1474cfb3ab26f53
inunotaisho26 <ebfairweather@gmail.com>
2018-04-24 20:44:07 -0400

    finished refactoring file info to c-sharp

--- path-autocomplete/Src/Features/FileInfo.cs
+++ path-autocomplete/Src/Features/FileInfo.cs
@@ -1,27 +1,25 @@
 ﻿using System;
 using System.IO;
 
-namespace pathautocomplete.Src.FileInfo
+namespace PathAutoComplete.Src.FileInfo
 {
     public class FileInfo
     {
         private string type;
-        private string nameExt;
-        private string nameNoExt;
+        private string name;
         private string itemPath;
+        private const string loc = "";
 
-        public FileInfo()
+        public FileInfo(string itemPath)
         {
-            var loc = '';
             this.itemPath = itemPath;
-            this.type = fs.statSync(itemPath).isDirectory() ? 'dir' : 'file';
-            this.nameExt = Path.GetFileName(itemPath);
-            this.nameNoExt = Path.GetFileNameWithoutExtension(itemPath);
+            this.type = Directory.Exists(itemPath) ? "dir" : "file";
+            this.name = (Directory.Exists(itemPath)) ? Path.GetDirectoryName(itemPath) : Path.GetFileName(itemPath);
         }
 
         bool isDirectory()
         {
-            return this.type == 'dir';
+            return this.type == "dir";
         }
 
         string getPath()
@@ -31,7 +29,7 @@ namespace pathautocomplete.Src.FileInfo
 
         string getName()
         {
-            return (this.nameExt, this.nameNoExt);
+            return this.name;
         }
     }
 }
