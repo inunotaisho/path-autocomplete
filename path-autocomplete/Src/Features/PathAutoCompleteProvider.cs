fd5acee3a2908690457bb861d85f87709f406ecf
inunotaisho26 <ebfairweather@gmail.com>
2018-04-24 20:45:06 -0400

    beginning refactoring Provider

--- path-autocomplete/Src/Features/PathAutoCompleteProvider.cs
+++ path-autocomplete/Src/Features/PathAutoCompleteProvider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using pathautocomplete.Src.FileInfo;
+using System.Text.RegularExpressions;
+using PathAutoComplete.Src.FileInfo;
+using PathAutoComplete.Src.Configuration;
 
 
 namespace pathautocomplete.Src.PathAutoCompleteProvider
@@ -7,8 +9,31 @@ namespace pathautocomplete.Src.PathAutoCompleteProvider
     public class PathAutoCompleteProvider
     {
         string currentFile;
+
+        var configuration = new PathConfiguration();
         public PathAutoCompleteProvider()
         {
+
+            var self = this;
         }
+
+        //Builds a list of the available files and folders from the provided path.
+
+        getFolderItems(string FolderPath){
+
+
+            var results = null;
+        }
+
+        //Returns the current working directory
+
+        //Applies the folder mappings based on the user configurations
+
+        //Determine if the current path
+
+        //Determine if we should provide path completion.
+
+        //Filter for the suggested items
+     
     }
 }
